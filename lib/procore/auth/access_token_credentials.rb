1c5e130f911191929dce48b12c154e92e95dfc1a
Michael Stock <mikeastock@gmail.com>
2018-01-04 16:40:41 -0800

    feat: Wrap AccessTokenCredentials in our own errors
    This allows users to only have to worry about our gem's errors.
    
    This resolves #5

--- lib/procore/auth/access_token_credentials.rb
+++ lib/procore/auth/access_token_credentials.rb
@@ -11,14 +11,28 @@ module Procore
       end
 
       def refresh(token:, refresh:)
-        token = OAuth2::AccessToken.new(client, token, refresh_token: refresh)
-        new_token = token.refresh!
+        begin
+          token = OAuth2::AccessToken.new(client, token, refresh_token: refresh)
+          new_token = token.refresh!
 
-        Procore::Auth::Token.new(
-          access_token: new_token.token,
-          refresh_token: new_token.refresh_token,
-          expires_at: new_token.expires_at,
-        )
+          Procore::Auth::Token.new(
+            access_token: new_token.token,
+            refresh_token: new_token.refresh_token,
+            expires_at: new_token.expires_at,
+          )
+
+        rescue OAuth2::Error => e
+          raise OAuthError.new(e.description, response: e.response)
+
+        rescue Faraday::ConnectionFailed => e
+          raise APIConnectionError.new("Connection Error: #{e.message}")
+
+        rescue URI::BadURIError
+          raise OAuthError.new(
+            "Host is not a valid URI. Check your host option to make sure it " \
+            "is a properly formed url",
+          )
+        end
       end
 
       private
