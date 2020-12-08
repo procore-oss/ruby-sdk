5872b1150132ad603c6c7e536fc3d59d875320d3
Nate Baer <njbbaer@gmail.com>
2020-12-08 14:10:43 -0800

    Fix config doc comment

--- lib/procore/configuration.rb
+++ lib/procore/configuration.rb
@@ -37,7 +37,7 @@ module Procore
     attr_accessor :host
 
     # @!attribute [rw] default_version
-    # @note defaults to Defaults::API_ENDPOINT
+    # @note required attribute
     #
     # The default API version to use if none is specified in the request.
     # Should be either "v1.0" (recommended) or "vapid" (legacy).
